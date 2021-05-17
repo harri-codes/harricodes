8cb18e94ce81c8686a6e43f76d5396054388840e
Harrison Njeru <harrisonnjeru7@gmail.com>
2021-05-17 08:25:54 +0300

    first commit

---
+++ cli.js
@@ -0,0 +1,112 @@
+#!/usr/bin/env node
+
+'use strict'
+
+const boxen = require("boxen");
+const chalk = require("chalk");
+const inquirer = require("inquirer");
+const clear = require("clear");
+const open = require("open");
+const fs = require('fs');
+const request = require('request');
+const path = require('path');
+const ora = require('ora');
+const cliSpinners = require('cli-spinners');
+clear();
+
+const prompt = inquirer.createPromptModule();
+
+const questions = [
+    {
+        type: "list",
+        name: "action",
+        message: "Hello What do you want to do?",
+        choices: [
+            {
+                name: `Send me an ${chalk.green.bold("email")}?`,
+                value: () => {
+                    open("mailto:harrisonnjeru7@gmail.com");
+                    console.log("\nDone, see you soon at inbox.\n");
+                }
+            },
+            {
+                name: `Reach via ${chalk.green.bold("call")}?`,
+                value: () => {
+                    open("tel:+254746998778");
+                    console.log("\nRinging.\n");
+                }
+            },
+            {
+                name: "Just quit.",
+                value: () => {
+                    console.log("Hasta la vista.\n");
+                }
+            }
+        ]
+    }
+];
+
+const data = {
+    name: chalk.bold.green("             Harrison Njeru"),
+    handle: chalk.white("@harricodes"),
+    work: `${chalk.white("Senior Software Engineer at")} ${chalk
+        .hex("#2b82b2")
+        .bold("Geeksrepubliq")}`,
+    twitter: chalk.gray("https://twitter.com/") + chalk.cyan("harricodes"),
+    github: chalk.gray("https://github.com/") + chalk.green("harricodes"),
+    linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("harricodes"),
+    web: chalk.cyan("https://harricodes.me"),
+    npx: chalk.red("npx") + " " + chalk.white("harricodes"),
+
+    labelWork: chalk.white.bold("       Work:"),
+    labelTwitter: chalk.white.bold("    Twitter:"),
+    labelGitHub: chalk.white.bold("     GitHub:"),
+    labelLinkedIn: chalk.white.bold("   LinkedIn:"),
+    labelWeb: chalk.white.bold("        Web:"),
+    labelCard: chalk.white.bold("       Card:")
+};
+
+const me = boxen(
+    [
+        `${data.name}`,
+        ``,
+        `${data.labelWork}  ${data.work}`,
+        ``,
+        `${data.labelTwitter}  ${data.twitter}`,
+        `${data.labelGitHub}  ${data.github}`,
+        `${data.labelLinkedIn}  ${data.linkedin}`,
+        `${data.labelWeb}  ${data.web}`,
+        ``,
+        `${data.labelCard}  ${data.npx}`,
+        ``,
+        `${chalk.italic(
+            "I am looking for new opportunities,"
+        )}`,
+        `${chalk.italic("always willing to collaborate")}`,
+        `${chalk.italic("my inbox is always open. Whether you have a")}`,
+        `${chalk.italic(
+            "question or just want to say hi, I will try "
+        )}`,
+        `${chalk.italic(
+            "my best to get back to you!"
+        )}`
+    ].join("\n"),
+    {
+        margin: 1,
+        float: 'center',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }
+);
+
+console.log(me);
+const tip = [
+    `Tip: Try ${chalk.cyanBright.bold(
+        "cmd/ctrl + click"
+    )} on the links above`,
+    '',
+].join("\n");
+console.log(tip);
+
+prompt(questions).then(answer => answer.action());
